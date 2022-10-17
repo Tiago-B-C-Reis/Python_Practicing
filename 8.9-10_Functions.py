96e6d66498ba3864468a9f9bfa4befa5ff6f17c1
Tiago-B-C-Reis <tiago.b.c.reis@hotmail.com>
2022-10-17 21:17:53 +0100

    Status_17-10-2022

---
+++ 8.9-10_Functions.py
@@ -0,0 +1,13 @@
+# 8.9
+
+
+def show_messages(message):
+
+    for item in message_list:
+        print(item)
+
+
+message_list = ["Hello", "Good morning", "Good afternoon", "Good evening", "Good by"]
+show_messages(message_list)
+
+
