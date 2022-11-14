8d54706df8d1efcefcb36dace3c4f9523e0cfd0d
Tiago-B-C-Reis <tiago.b.c.reis@hotmail.com>
2022-11-14 16:11:00 +0000

    Stage_14-11-2022

---
+++ Projects/Project_I_Alien_Invasion/adding_ship_image/button.py
@@ -0,0 +1,36 @@
+import pygame.font
+
+
+class Button:
+    """Creates and manages the button to start and stop the
+    game with user input."""
+
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        # The None argument tells Pygame to use the default font,
+        # and 48 specifies the size of the text.
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # The button message needs to be prepped only once.
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        """Turns msg into a rendered image and center text on the button"""
+        # The call to font.render() turns the text stored in
+        # msg into an image, which we then store in self.msg_image
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                                          self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
